f2cdd8cd964744b5ca281608d13e2aec773a0e13
HamedHaddadi <haddadi.h81@gmail.com>
2023-12-15 19:09:16 -0500

    configured for linode servers

--- sc_src/source/utils/market_data.py
+++ sc_src/source/utils/market_data.py
@@ -4,13 +4,13 @@ from pandas_datareader.nasdaq_trader import get_nasdaq_symbols
 import yfinance as yf 
 import requests 
 import re 
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 from . import tools    
 from . import keys 
 from datetime import date, datetime
 import time 
 import re 
-import fitz 
+#import fitz 
 from os import path 
 # ...
 
@@ -84,45 +84,45 @@ def parse_row(row, asset_fund = None):
                 asset_fund[key] = float(values[0])*factor 
     return asset_fund 
         
-def pull_fundamentals_finviz(stock_list = None):
-    if 'sp500' in stock_list:
-        _,stock_list = pull_sp500_tickers()
-    else:
-        raise Exception('stock list is None')
+# def pull_fundamentals_finviz(stock_list = None):
+#     if 'sp500' in stock_list:
+#         _,stock_list = pull_sp500_tickers()
+#     else:
+#         raise Exception('stock list is None')
 
-    total_length = len(stock_list)
-    BATCH_SIZE = 4
-    stock_batch = tools.batched(stock_list, BATCH_SIZE)
-    url_base = 'https://finviz.com/quote.ashx?t='
-    request_headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:76.0) Gecko/20100101 Firefox/76.0'}
+#     total_length = len(stock_list)
+#     BATCH_SIZE = 4
+#     stock_batch = tools.batched(stock_list, BATCH_SIZE)
+#     url_base = 'https://finviz.com/quote.ashx?t='
+#     request_headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:76.0) Gecko/20100101 Firefox/76.0'}
 
-    list_df = []
-    downloaded = 0 
-    for tickers in stock_batch:
-        time.sleep(20)
-        print(f'start downloading {tickers} after the pause; {downloaded} out of {total_length} so far')
-        for ticker in tickers:
-            asset_url = url_base + ticker  
-            html_soup = BeautifulSoup(requests.get(asset_url, headers = request_headers).content, 'html.parser')
-            asset_fund = {'P/E': None, 'EPS-ttm': None, 'Shares Outstanding': None, 
-                'Market Cap': None, 'Floating Shares': None, 
-                        'PEG': None, 'P/S': None, 'Book/Share Ratio': None, 
-                            'ROA': None, 'Dividend $': None, 'Dividend %': None, 
-                                'D/E': None, 'Stock': ticker}
+#     list_df = []
+#     downloaded = 0 
+#     for tickers in stock_batch:
+#         time.sleep(20)
+#         print(f'start downloading {tickers} after the pause; {downloaded} out of {total_length} so far')
+#         for ticker in tickers:
+#             asset_url = url_base + ticker  
+#             html_soup = BeautifulSoup(requests.get(asset_url, headers = request_headers).content, 'html.parser')
+#             asset_fund = {'P/E': None, 'EPS-ttm': None, 'Shares Outstanding': None, 
+#                 'Market Cap': None, 'Floating Shares': None, 
+#                         'PEG': None, 'P/S': None, 'Book/Share Ratio': None, 
+#                             'ROA': None, 'Dividend $': None, 'Dividend %': None, 
+#                                 'D/E': None, 'Stock': ticker}
         
-            for row in html_soup.select('.snapshot-table2 tr'):
-                asset_fund = parse_row(row, asset_fund=asset_fund)
+#             for row in html_soup.select('.snapshot-table2 tr'):
+#                 asset_fund = parse_row(row, asset_fund=asset_fund)
         
-            asset_df = pd.DataFrame([asset_fund])
-            if list(asset_df.columns) == list(asset_fund.keys()):
-                list_df.append(asset_df)
-        downloaded += len(list_df)*BATCH_SIZE
-    list_df = pd.concat(list_df)
-    save_name = 'list_of_stocks_fundamentals_from_finviz_on_' + datetime.now().strftime('%Y-%m-%d-%H-%M') + '.csv'
-    list_df.to_csv(path.join(keys.DATA_PATH, save_name), sep = ',', header = True, index = False, float_format = '%.4f')
+#             asset_df = pd.DataFrame([asset_fund])
+#             if list(asset_df.columns) == list(asset_fund.keys()):
+#                 list_df.append(asset_df)
+#         downloaded += len(list_df)*BATCH_SIZE
+#     list_df = pd.concat(list_df)
+#     save_name = 'list_of_stocks_fundamentals_from_finviz_on_' + datetime.now().strftime('%Y-%m-%d-%H-%M') + '.csv'
+#     list_df.to_csv(path.join(keys.DATA_PATH, save_name), sep = ',', header = True, index = False, float_format = '%.4f')
 
-def pull_fundamentals(source = 'finviz', stock_list = None):
-    {'finviz': pull_fundamentals_finviz}[source](stock_list = stock_list)
+# def pull_fundamentals(source = 'finviz', stock_list = None):
+#     {'finviz': pull_fundamentals_finviz}[source](stock_list = stock_list)
 
 
 # ### 		Data Frame Tools 		### #
@@ -170,25 +170,25 @@ def sp_sector(market_cap = None, pe = None, dividend = None):
 
 
 # ######### Useful methods for the list of Russel3000 ########### #
-def generate_russell3000_companies():
-    """
-    generates a dataframe of company name - ticker symbol for Russell3000
-    """
-    ru3000 = fitz.open(keys.RUSSELL3000_PDF)
-    text = []
-    for num in range(ru3000.page_count):
-        text.extend(ru3000.load_page(num).get_text('text').splitlines())
-    not_include = ['Russell US Indexes', 'Russell 3000® Index ', 'Membership list', 'Ticker', 'Company', 'June 24, 2022']
-    companies = {'companies': [], 'tickers': []}
-    for count, elem in enumerate(text):
-        if 'ftserussell.com' in elem:
-            break
-        if (len(elem.split(' ')) > 1 or bool(re.search(r'\d', elem)) or len(elem) > 4) and (elem.strip() not in not_include):
-            companies['companies'].append(elem)
-            companies['tickers'].append(text[count + 1])
-    comp_df = pd.DataFrame.from_dict(companies)
-    comp_filename = 'Russell3000_Companies.csv'
-    comp_df.to_csv(path.join(keys.DATA_PATH, comp_filename), sep = ',', header = True, index = False)
+# def generate_russell3000_companies():
+#     """
+#     generates a dataframe of company name - ticker symbol for Russell3000
+#     """
+#     ru3000 = fitz.open(keys.RUSSELL3000_PDF)
+#     text = []
+#     for num in range(ru3000.page_count):
+#         text.extend(ru3000.load_page(num).get_text('text').splitlines())
+#     not_include = ['Russell US Indexes', 'Russell 3000® Index ', 'Membership list', 'Ticker', 'Company', 'June 24, 2022']
+#     companies = {'companies': [], 'tickers': []}
+#     for count, elem in enumerate(text):
+#         if 'ftserussell.com' in elem:
+#             break
+#         if (len(elem.split(' ')) > 1 or bool(re.search(r'\d', elem)) or len(elem) > 4) and (elem.strip() not in not_include):
+#             companies['companies'].append(elem)
+#             companies['tickers'].append(text[count + 1])
+#     comp_df = pd.DataFrame.from_dict(companies)
+#     comp_filename = 'Russell3000_Companies.csv'
+#     comp_df.to_csv(path.join(keys.DATA_PATH, comp_filename), sep = ',', header = True, index = False)
         
 
     
